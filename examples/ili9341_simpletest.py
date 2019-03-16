f562078269103869935fb784a7fdc9d084c4d00d
Melissa LeBlanc-Williams <melissa@melissagirl.com>
2019-03-15 23:37:56 -0700

    Made pins easier to change

--- examples/ili9341_simpletest.py
+++ examples/ili9341_simpletest.py
@@ -8,11 +8,14 @@ import displayio
 import adafruit_ili9341
 
 spi = board.SPI()
+tft_cs = board.D9
+tft_dc = board.D10
+
 try:
-    display_bus = displayio.FourWire(spi, command=board.D10, chip_select=board.D9)
+    display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs)
 except ValueError:
     displayio.release_displays()
-    display_bus = displayio.FourWire(spi, command=board.D10, chip_select=board.D9)
+    display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs)
 
 display = adafruit_ili9341.ILI9341(display_bus)
 
